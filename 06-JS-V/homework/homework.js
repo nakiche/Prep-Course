47a8cdeb65488088ffed700ab0dbc4eabb9f9d54
nakiche <tommynaquiche@hotmail.com>
2022-06-04 11:29:31 -0500

    06-JS-V

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,12 +8,31 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+
+  function Usuario(opciones){
+    
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.password = opciones.password;
+
+    }
+
+    Usuario.prototype.saludar = function(){
+      return 'Hola, mi nombre es '+ this.nombre;
+    }  
+
+  return Usuario;  
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+
+  Constructor.prototype.saludar = function(){
+      return 'Hello World!';
+    }  
 }
 
 function agregarStringInvertida() {
@@ -22,6 +41,26 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+
+   String.prototype.reverse =function() {
+     
+       
+    // Paso 1. Usa el método split() para devolver un nuevo arreglo
+    var separarCadena = this.split(""); // var separarCadena = "hola".split("");
+    // ["h", "o", "l", "a"]
+ 
+    // Paso 2. Usa el método reverse() para invertir el nuevo arreglo creado
+    var invertirArreglo = separarCadena.reverse(); // var invertirArreglo = ["h", "o", "l", "a"].reverse();
+    // ["a", "l", "o", "h"]
+ 
+    // Paso 3. Usa el método join() para unir todos los elementos del arreglo en una cadena
+    var unirArreglo = invertirArreglo.join(""); // var unirArreglo = ["a", "l", "o", "h"].join("");
+    // "aloh"
+    
+    //Paso 4. Devolver la cadena invertida
+    return unirArreglo; // "aloh"
+  
+  }
 }
 
 // ---------------------------------------------------------------------------//
@@ -36,21 +75,47 @@ function agregarStringInvertida() {
     //  }
 
   class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
+    constructor(nombre, apellido, edad, dir) {
       // Crea el constructor:
+      this.Nombre=nombre,
+      this.Apellido=apellido,
+      this.Edad=edad,
+      this.Domicilio=dir
 
+
+      this.detalle = function(){
+        return {
+          Nombre:this.Nombre,
+          Apellido: this.Apellido,
+          Edad: this.Edad,
+          Domicilio: this.Domicilio
+        }
+      }
     }
+
+
 }
 
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+
+ const sam = new Persona(nombre, apellido, edad, dir);
+
+ return sam;
+
+
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+
+Persona.prototype.datos = function () {
+  return this.Nombre + ', ' + this.Edad +' años';
+}
+
 }
   
 
