c54d22fa60c1826379d5be4cb9ce18279dcbea92
nakiche <tommynaquiche@hotmail.com>
2022-06-18 00:57:07 -0500

    10-JS-Extra-Homeworks-after watching help video

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -158,9 +158,16 @@ function sortArray(arr) {
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
 
-arr.sort(function(a, b) {
-  return a.length - b.length;
-});
+for(var i = 0; i < arr.length; i++) {
+    for(var j = i + 1; j < arr.length; j++) {
+      if (arr[i].length > arr[j].length)
+      {
+        var menor = arr[i];
+        arr[i]=arr[j];
+        arr[j]=menor;
+      }
+    }
+  }
 
 return arr;
 }
